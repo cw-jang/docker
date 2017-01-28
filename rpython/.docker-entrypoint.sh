080fb48a20b6f4862940e9d533ffc3e503e094a4
cwjang <changwan.jang@gmail.com>
2017-01-28 12:30:55 +0900

    rpython 등록

---
+++ rpython/.docker-entrypoint.sh
@@ -0,0 +1,13 @@
+# delete password
+unset USER_PASS
+
+# start supervisord
+#/home/$USER_ID/anaconda2/bin/supervisord -c /etc/supervisord.conf
+supervisord -c /etc/supervisord.conf
+
+# enable ipyparallel
+#cd /home/$USER_ID/.jupyter
+#sudo -u $USER_ID /home/$USER_ID/anaconda2/bin/ipcluster nbextension enable
+
+# user login
+sudo -u $USER_ID -i '/bin/bash'
