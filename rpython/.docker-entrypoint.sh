1193ba1c565681a776b6b24a1404d684ee3c0124
cwjang <changwan.jang@gmail.com>
2017-01-30 18:50:00 +0900

    mysql 추가

--- rpython/.docker-entrypoint.sh
+++ rpython/.docker-entrypoint.sh
@@ -3,11 +3,11 @@ unset USER_PASS
 
 # start supervisord
 #/home/$USER_ID/anaconda2/bin/supervisord -c /etc/supervisord.conf
-supervisord -c /etc/supervisord.conf
+supervisord -n -c /etc/supervisord.conf
 
 # enable ipyparallel
 #cd /home/$USER_ID/.jupyter
 #sudo -u $USER_ID /home/$USER_ID/anaconda2/bin/ipcluster nbextension enable
 
 # user login
-sudo -u $USER_ID -i '/bin/bash'
+#sudo -u $USER_ID -i '/bin/bash'
